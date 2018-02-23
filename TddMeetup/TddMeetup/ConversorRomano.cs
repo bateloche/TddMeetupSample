4b2afa1bd34bad6d5039ed69a81311859c84cd30
bateloche <bateloche@outlook.com>
2018-02-23 20:59:04 -0300

    Initial commit

---
+++ TddMeetup/TddMeetup/ConversorRomano.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddMeetup
+{
+    public class ConversorRomano
+    {
+        private Dictionary<char, int> _algarismos =
+            new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 }
+            };
+
+        public int Converte(string romano)
+        {
+            var valor = 0;
+
+            var anterior = romano[0];
+
+            for (var i = 1; i < romano.Length; i++)
+            {
+                var atual = romano[i];
+
+            }
+
+            foreach (var algarismo in romano)
+            {
+                valor += _algarismos[algarismo];
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ TddMeetup/TddMeetup/ConversorRomanoTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TddMeetup
+{
+    public class ConversorRomanoTests
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("V", 5)]
+        [InlineData("X", 10)]
+        public void Deve_Entender_Um_Numero_De_Um_Algarismo(string romano, int esperado)
+        {
+            var conversorRomano = new ConversorRomano();
+
+            var resultado = conversorRomano.Converte(romano);
+
+            Assert.Equal(esperado, resultado);
+        }
+
+        [Theory]
+        [InlineData("II", 2)]
+        [InlineData("VI", 6)]
+        [InlineData("XI", 11)]
+        public void Deve_Entender_Se_Um_Numero_Tem_Dois_Algarismos(string romano, int esperado)
+        {
+            var conversorRomano = new ConversorRomano();
+
+            var resultado = conversorRomano.Converte(romano);
+
+            Assert.Equal(esperado, resultado);
+        }
+
+        [Theory]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        public void Deve_Entender_Quando_O_Algarismo_Da_Esquerda_For_Menor_Que_O_Da_Direita(string romano, int esperado)
+        {
+            var conversorRomano = new ConversorRomano();
+
+            var resultado = conversorRomano.Converte(romano);
+
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
